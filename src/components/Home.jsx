0234d8c21dc57330e718cb2dce0e7b73f21342b3
Pablo <47283527+PabloSR06@users.noreply.github.com>
2023-09-28 18:06:45 +0100

    wip: working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, FlatList } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
 import { listFiles, readFileContent } from '../utils/filesDb';
-import { Button, Icon } from 'react-native-elements';
+import { Button, Icon, ListItem } from 'react-native-elements';
 import { HomeStyle } from '../utils/styles/style';
 import { ActivityIndicator } from 'react-native-paper';
+import Menu, {
+  MenuProvider,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger,
+} from 'react-native-popup-menu';
 
 const Home = ({ navigation }) => {
   const [files, setFiles] = useState([]);
@@ -20,49 +26,99 @@ const Home = ({ navigation }) => {
     init();
   }, []);
 
-  const renderFileItem = ({ item }) => {
-    const timestamp = new Date(parseInt(item));
-    const formattedDate = timestamp.toLocaleDateString();
+  const onTriggerPress = (index) => {
+    console.log('onTriggerPress');
+  }
 
-    const handlePress = () => {
-      const itemName = item.replace('.json', '');
-      navigation.navigate('Play', {
-        screen: 'Game',
-        params: { itemName },
-      });
-    };
-    const handleHold = () => {
-      const itemName = item.replace('.json', '');
-      navigation.navigate('GenerateQr', itemName);
-    };
+  const onBackdropPress = (index) => {
+    console.log('onBackdropPress');
+  }
+
+  const onOptionSelect = (value, index) => {
+    alert(`Selected number: ${value}`);
+    const list = [...this.state.list];
+    list[index].isOpen = false;
+    this.setState({ list });
+  }
+
+  const renderItem = ({ item, index }) => (
+    <View  >
+      <Text>sad</Text>
+      <ListItem
+        
+        onPress={() => console.log('onPress')}
+        key={index}
+        iconStyle={{ color: 'red' }}
+        title={
+          <View>
+            <Text style={{ fontWeight: 'bold' }}>adfgadfgadfgadfg</Text>
+            <Text style={{ fontWeight: 'bold' }}>adfgadfgadfgadfg</Text>
+            <Text style={{ fontWeight: 'bold' }}>adfgadfgadfgadfg</Text>
+            <Text style={{ fontWeight: 'bold' }}>adfgadfgadfgadfg</Text>
+            <Text style={{ fontWeight: 'bold' }}>adfgadfgadfgadfg</Text>
+            <Text style={{ fontWeight: 'bold' }}>adfgadfgadfgadfg</Text>
+            <Text style={{ fontWeight: 'bold' }}>adfgadfgadfgadfg</Text>
+            
+          </View>
+        }
+        subtitle={item}
 
 
-    return (
-      <TouchableOpacity onLongPress={handleHold} onPress={handlePress} style={HomeStyle.fileItem}>
-        <Text>{formattedDate}</Text>
-      </TouchableOpacity>
-    );
-  };
-  const [loading, setLoading] = useState(true);
-
-  return (
-    <View style={HomeStyle.container}>
-      <View >
-
-             
-
-        <Button title="New Game" onPress={() => setLoading(!loading)} />
-      </View>
-
-      <Text style={HomeStyle.heading}>Last games</Text>
-      <FlatList
-        data={files}
-        keyExtractor={(item) => item}
-        renderItem={renderFileItem}
       />
     </View>
   );
+  const getMenuView = (index, isOpen) => {
+    return (
+      <MenuProvider style={{ flexDirection: 'column', padding: 30 }}>
+        <Menu opened={isOpen}>
+          <MenuTrigger
+            onBackdropPress={() => onBackdropPress(index)}
+            onSelect={value => onOptionSelect(value, index)}
+          >
+            <Icon
+              onPress={() => onTriggerPress(index)}
+              type="material"
+              color="red"
+              name="more-vert"
+            />
+          </MenuTrigger>
+          <MenuOptions>
+            <MenuOption value={1} text="One" />
+            <MenuOption value={2}>
+              <Text style={{ color: 'red' }}>Two</Text>
+            </MenuOption>
+            <MenuOption value={3} disabled={true} text="Three" />
+          </MenuOptions>
+        </Menu>
+      </MenuProvider>
+    );
+  }
+
+
+  return (
+    <View>
+      <View>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={files}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={renderItem}
+        />
+      </View>
+    </View>
+
+  );
 };
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 8,
+  },
+});
+
 
 
 export default Home;
